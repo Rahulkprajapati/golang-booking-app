b6a18f142b17b11a64d3efa864483c09418110db
Rahulkrajapati <rahulkpkprajapati147@gmail.com>
2023-07-22 21:51:32 +0530

    Taking diff user inputs

--- main.go
+++ main.go
@@ -12,13 +12,25 @@ func main() {
 	fmt.Printf("We have total numbers of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your Ticket Now")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
 	// Ask User Input
-	fmt.Scan(&userName)
+	fmt.Println("Please Enter Your First Name:  ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please Enter Your Last Name:  ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please Enter Your Email:  ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter Number of Tickets You Want to Book:  ")
+	fmt.Scan(&userTickets)
+
 	// Here in Golang variables are stored in memory and we can access them using their memory address vai adding "&" pointers which acts special var to store memory store-> Hashtable
 
-	userTickets = 5
-	fmt.Printf("Hello %v, you have booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets for %v conference. Your tickets are booked and you will receive a confirmation email on %v\n", firstName, lastName, userTickets, conferenceName, email)
 }
