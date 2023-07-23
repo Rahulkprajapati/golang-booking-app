99c533f219fb8903c1a5913c0a522783e1939c01
Rahulkrajapati <rahulkpkprajapati147@gmail.com>
2023-07-23 20:58:20 +0530

    for loop

--- main.go
+++ main.go
@@ -11,43 +11,44 @@ func main() {
 	// var bookings []string
 	bookings := []string{}
 
-
 	fmt.Printf("Data Types of conferenceName is: %T , conferenceTickets is: %T and remainingTickets is: %T\n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Printf("Welcome to our %v booking Application\n", conferenceName)
 	fmt.Printf("We have total numbers of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your Ticket Now")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// Ask User Input
-	fmt.Println("Please Enter Your First Name:  ")
-	fmt.Scan(&firstName)
+		// Ask User Input
+		fmt.Println("Please Enter Your First Name:  ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please Enter Your Last Name:  ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please Enter Your Last Name:  ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please Enter Your Email:  ")
-	fmt.Scan(&email)
+		fmt.Println("Please Enter Your Email:  ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter Number of Tickets You Want to Book:  ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter Number of Tickets You Want to Book:  ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	//bookings[0] = firstName + " " + lastName
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		//bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Whole Booking Array is: %v\n", bookings)
-	fmt.Printf("First Booking is: %v\n", bookings[0])
-	fmt.Printf("Slice type is: %T\n", bookings)
-	fmt.Printf("Slice Length is: %v\n", len(bookings))
+		fmt.Printf("Whole Booking Array is: %v\n", bookings)
+		fmt.Printf("First Booking is: %v\n", bookings[0])
+		fmt.Printf("Slice type is: %T\n", bookings)
+		fmt.Printf("Slice Length is: %v\n", len(bookings))
 
-	// Here in Golang variables are stored in memory and we can access them using their memory address vai adding "&" pointers which acts special var to store memory store-> Hashtable
+		// Here in Golang variables are stored in memory and we can access them using their memory address vai adding "&" pointers which acts special var to store memory store-> Hashtable
 
-	fmt.Printf("Thank you %v %v for booking %v tickets for %v conference. Your tickets are booked and you will receive a confirmation email on %v\n", firstName, lastName, userTickets, conferenceName, email)
-	fmt.Printf("Remaining Tickets are: %v for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets for %v conference. Your tickets are booked and you will receive a confirmation email on %v\n", firstName, lastName, userTickets, conferenceName, email)
+		fmt.Printf("Remaining Tickets are: %v for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 }
